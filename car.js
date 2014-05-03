d9d2b89419531cc34a3dbf6c5f17791bc4fe8432
stumbledog <stumbledog@gmail.com>
2014-05-03 19:43:51 -0400

    @edward trying to put obj into GE

---
+++ car.js
@@ -0,0 +1,9 @@
+function Car(latitude, longitude, direction){
+	this.latitude = latitude;
+	this.longitude = longitude;
+	this.direction = direction;
+	this.speedLimit = 10;
+	this.steering = 0;
+	this.speed = 0;
+	this.power = 0;
+}
